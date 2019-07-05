a25afa50d19edaa3e35cfd4f4e7798c6795a9ae3
Manmeet Singh Parmar <manietdavv@gmail.com>
2019-07-05 23:07:58 +0530

    Update Cossack_A.cpp

--- Codeforces/571_contest/Cossack_A.cpp
+++ Codeforces/571_contest/Cossack_A.cpp
@@ -10,3 +10,19 @@ int main()
 		cout << "No" << endl;
 	return 0;
 }
+
+
+/* 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+	if(n <= m && n <= k)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	return 0;
+}
+*/
