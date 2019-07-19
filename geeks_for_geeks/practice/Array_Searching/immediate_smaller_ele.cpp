bad01f3ad0ceaabe45b0ed6f5f9c47dccb3a4c4e
Manmeet2018 <manietdavv@gmail.com>
2019-07-20 00:05:56 +0530

    Added_practice_material

--- geeks_for_geeks/practice/Array_Searching/immediate_smaller_ele.cpp
+++ geeks_for_geeks/practice/Array_Searching/immediate_smaller_ele.cpp
@@ -3,25 +3,23 @@
 using namespace std;
 
 int main() {
-int t;
-cin >>t;
-for(int i=0; i < t; i++)
-{
-int size;
-cin >>size;
-int *arr = new int[size];
-for(int j = 0 ; j < size; j++)
-{
-cin >> arr[j];
-}
-for(int j=0; j < size -1; j++)
-{
-if(arr[j] > arr[j+1])
-cout << arr[j+1] << " ";
-else
-cout << "-1 ";
-}
-cout << "-1" << endl;
-}
-return 0;
+    int t;
+    cin >>t;
+    for(int i=0; i < t; i++)
+    {
+        int size;
+        cin >>size;
+        int *arr = new int[size];
+        for(int j = 0 ; j < size; j++)
+            cin >> arr[j];
+        for(int j=0; j < size -1; j++)
+    {
+        if(arr[j] > arr[j+1])
+            cout << arr[j+1] << " ";
+        else
+            cout << "-1 ";
+    }
+        cout << "-1" << endl;
+    }
+    return 0;
 }
