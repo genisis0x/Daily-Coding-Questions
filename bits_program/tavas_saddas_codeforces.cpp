b990d69009b0ff154560ff5851ead22264e87f20
Manmeet2018 <manietdavv@gmail.com>
2019-08-03 20:07:47 +0530

    Added_B_codeforces_travas

---
+++ bits_program/tavas_saddas_codeforces.cpp
@@ -0,0 +1,59 @@
+// https://codeforces.com/problemset/problem/535/B
+
+// Created by Manmeet Singh Parmar
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, x, n) for(ll i=x; i<(n); i++)
+#define F0R(i, n)  for(ll i=0; i<(n); i++)
+#define FORd(i, x, n) for(ll i = (n)-1; i >= x; i--)
+#define F0Rd(i,n) for(ll i = (n)-1; i >= 0; i--)
+#define FOR_B_l(i,n) for(ll i = 0; i < (1 << n); i++)
+#define FOR_B_r(i,n) for(ll i = 0; i < (1 >> n); i++)
+
+
+#define l_rot_n(a, n) (a << n)
+#define r_rot_n(a, n) (a >> n)
+
+#define mod 1000000007
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+using namespace std;
+
+
+int main()
+{
+   	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+    string str;
+    cin>>str;
+    int n = str.length();
+    int rank = 0;
+    F0R(i, n)
+    {
+    	rank += (1 << i);
+    }
+    F0R(i, n)
+    {
+    	if(str[i] == '7')
+    		rank += (1 << (n - i - 1));
+    }
+    cout<<rank<<endl;
+    return 0;
+}
