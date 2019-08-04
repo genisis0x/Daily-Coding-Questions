1679e8f080257fa7b0759649dc8890df4b70bb75
Manmeet2018 <manietdavv@gmail.com>
2019-08-04 19:19:20 +0530

    Done_06

---
+++ Project_Eular/06.cpp
@@ -0,0 +1,53 @@
+// Created by Manmeet Singh Parmar
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, x, n) for(ll i=x; i<(n); i++)
+#define F0R(i, n)  for(ll i=0; i<(n); i++)
+#define FORd(i, x, n) for(ll i = (n)-1; i >= x; i--)
+#define F0Rd(i,n) for(ll i = (n)-1; i >= 0; i--)
+#define FOR_B_l(i,n) for(ll i = 0; i < (1 << n); i++)
+#define FOR_B_r(i,n) for(ll i = 0; i < (1 >> n); i++)
+
+
+#define l_rot_n(a, n) (a << n)
+#define r_rot_n(a, n) (a >> n)
+
+#define mod 1000000007
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+using namespace std;
+
+
+ll sum_diff(int n)
+{
+	ll a = (n*(n + 1)) / 2;
+	ll b = ((3 * n * n) - n - 2) / 6;
+	return a * b;
+}
+
+int main()
+{
+   	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n;
+	cin >>n;
+	cout<<sum_diff(n)<<endl;
+    return 0;
+}
