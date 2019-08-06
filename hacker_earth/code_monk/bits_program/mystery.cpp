0d5f64022096faeb3520dd3c9c5893a6fd817041
Manmeet2018 <manietdavv@gmail.com>
2019-08-06 18:02:03 +0530

    Added_bit_program

---
+++ hacker_earth/code_monk/bits_program/mystery.cpp
@@ -0,0 +1,66 @@
+// PASSED
+// https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/mystery-30/
+// Created by Manmeet Singh Parmar
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, x, n) for(ll i=x; i<(n); i++)
+#define F0R(i, n)  for(ll i=0; i<(n); i++)
+#define FORd(i, x, n) for(ll i = (n)-1; i >= x; i--)
+#define F0Rd(i,n) for(ll i = (n)-1; i >= 0; i--)
+#define FOR_B_l(i,n) for(ll i = 0; i < (1LL << n); i++)
+#define FOR_B_r(i,n) for(ll i = 0; i < (1LL >> n); i++) // typecasting of 1 in long long
+
+
+#define l_rot_n(a, n) (aLL << n) // type casting of a in long long and lef rotation of a with n;
+
+#define r_rot_n(a, n) (aLL >> n)
+
+#define mod 1000000007
+#define pi 2acos(0.0)
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back // its's faster than push_back
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+#define what_is(x) cout << #x << "is" << x << endl;
+#define gcd(a, b) __gcd(num1 , num2)
+using namespace std;
+
+
+int count_bits(ll n)
+{
+    int c = 0;
+    while(n)
+    {
+        c++;
+        n &= n-1;   
+    }
+    return c;
+}
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll n;
+     while(cin>>n) // used to read the input when it reaches the End of file (not finite no of test cases are defined)
+         cout<<count_bits(n)<<"\n";
+     return 0;
+}
