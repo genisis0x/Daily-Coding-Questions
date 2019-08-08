123daefb3a9405aaa44cddee33e0c5598a79af62
Manmeet2018 <manietdavv@gmail.com>
2019-08-08 17:44:19 +0530

    Added_Birthday_paradox

---
+++ programs/Birthday_paradox.cpp
@@ -0,0 +1,65 @@
+// Created by Manmeet Singh Parmar
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, x, n) for(ll i=x; i<(n); i++)
+#define F0R(i, n)  for(ll i=0; i<(n); i++)
+#define FORd(i, x, n) for(ll i = (n)-1; i >= x; i--)
+#define F0Rd(i,n) for(ll i = (n)-1; i >= 0; i--)
+#define FOR_B_l(i,n) for(ll i = 0; i < (1LL << n); i++)
+#define FOR_B_r(i,n) for(ll i = 0; i < (1LL >> n); i++) // typecasting of 1 in long long
+
+
+#define l_rot_n(a, n) (aLL << n) // type casting of a in long long and lef rotation of a with n;
+
+#define r_rot_n(a, n) (aLL >> n)
+
+#define mod 1000000007
+#define pi 2acos(0.0)
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back // its's faster than push_back
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+#define what_is(x) cerr << #x << "is" << x << endl;
+#define gcd(a, b) __gcd(num1 , num2)
+using namespace std;
+
+int main()
+{
+   	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    float per; // the percentage which we need
+    cin>>per;
+    per /=100;
+    float num = 365;
+    float denom = 365;
+
+    float p = 1;
+    int n = 0;
+    while((1 - p) < per) // (1 -p) // prob of people having same birthday.
+    {
+    	p = p*(num)/denom; // prob maker for people having different birthday
+    	num--;
+    	n++;
+    }
+    cout <<n<<"\n";
+    return 0;
+}
