5188cbf05c1f5979e526acd35c0d17b2e8fabefa
Manmeet2018 <manietdavv@gmail.com>
2019-09-05 18:13:05 +0530

    Array_\bits_pratice

--- interview_bit/array/conti_sub_array.cpp
+++ interview_bit/array/conti_sub_array.cpp
@@ -2,11 +2,11 @@
 // https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
 int Solution::maxSubArray(const vector<int> &a) 
 {
-	int cur_max, glob_max = a[0];
-	for(int i = 1; i < a.size(); i++)
-	{
-		cur_max = max(a[i], a[i] + cur_max);
-		glob_max = (cur_max > glob_max) ? cur_max : glob_max;
-	}
-	return glob_max;
+    int cur_max = a[0]; int glob_max = a[0];
+    for(int i = 1; i < a.size(); i++)
+    {
+        cur_max = max(a[i], a[i] + cur_max);
+        glob_max = max(glob_max, cur_max);
+    }
+    return glob_max;
 }
