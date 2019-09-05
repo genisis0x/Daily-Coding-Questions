5f80ee1bc1e7fecc8c1711fb8dd8a5944b31f77e
Manmeet2018 <manietdavv@gmail.com>
2019-09-05 17:33:23 +0530

    Array_pratice

---
+++ interview_bit/array/Add_One_To_Number.cpp
@@ -0,0 +1,33 @@
+// Passed
+// https://www.interviewbit.com/problems/add-one-to-number/
+
+vector<int> Solution::plusOne(vector<int> &A) 
+{
+    int n = A.size();
+    int carry = 0;
+    vector<int> arr(n + 1);
+    arr[n] = A[n -1] + 1;
+    for(int i = n; i>0; i--)
+    {
+        arr[i] += carry;
+        if(arr[i] > 9)
+        {
+            carry = arr[i] / 10;
+            arr[i] %= 10;
+        }
+        else
+            carry =0;
+        if(i != 1)
+            arr[i - 1] = A[i - 2];
+    }
+    if(carry)
+        arr[0] = carry;
+    int i = 0;
+    while(arr[i] == 0)
+    {
+        vector<int>::iterator it; 
+        it = arr.begin(); 
+        arr.erase(arr.begin());
+    }
+    return arr;
+}
---
+++ interview_bit/array/conti_sub_array.cpp
@@ -0,0 +1,12 @@
+// Passed
+// https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
+int Solution::maxSubArray(const vector<int> &a) 
+{
+	int cur_max, glob_max = a[0];
+	for(int i = 1; i < a.size(); i++)
+	{
+		cur_max = max(a[i], a[i] + cur_max);
+		glob_max = (cur_max > glob_max) ? cur_max : glob_max;
+	}
+	return glob_max;
+}
