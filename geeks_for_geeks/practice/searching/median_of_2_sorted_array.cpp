88854d3ea52b00ff9287e1c200b65f4a82b980af
Manmeet2018 <manietdavv@gmail.com>
2019-09-12 01:54:40 +0530

    Searching_pratice

--- geeks_for_geeks/practice/searching/median_of_2_sorted_array.cpp
+++ geeks_for_geeks/practice/searching/median_of_2_sorted_array.cpp
@@ -10,6 +10,88 @@ void util(int arr1[], int arr2[], int n, int m)
 	   //     cout<<arr1[i]<<" ";
 	   // for(int i=0; i<m; i++)
 	   //     cout<<arr2[i]<<" ";
+    int flag = 0;
+    ll median = (n + m)/2 + 1;
+    if(!((n+m) & 1))
+    {
+        flag = 1;
+        median +=1;
+    }
+    // cout<<median<<"\n";
+    vector <int> v;
+    int i =0;
+    int j = 0;
+    int c = 0;
+    while(c < median)
+    {
+        if(arr1[i] >= arr2[j] && j<m && i<n)
+        {
+            v.push_back(arr2[j]);
+            c++;
+            j++;
+        }
+        else if(arr1[i] < arr2[j] && j<m && i<n)
+        {
+            v.push_back(arr1[i]);
+            c++;
+            i++;
+        }
+        else
+            break;
+    }
+    while(i<n && c < median)
+    {
+        v.push_back(arr1[i++]);
+        c++;
+    }
+    while(j<m && c < median){
+        v.push_back(arr2[j++]);
+        c++;
+    }
+    int e = v.size() -1;
+    // cout<<e<<"\n";
+    // for(int i=0; i<e; i++)
+    //     cout<<v[i]<<" ";
+    long double res = 0;
+    if(flag)
+        res = (v[e -1] + v[e -2])/2;
+    else
+        res = v[e];
+    cout<<floor(res)<<"\n";
+}
+
+
+int main()
+ {
+	int t; cin>>t;
+	while(t--){
+	    int n; cin>>n; int m; cin>>m;
+	    int arr1[n]; int arr2[m];
+	    for(int i=0; i<n; i++)
+	        cin>>arr1[i];
+	    for(int i=0; i<m; i++)
+	        cin>>arr2[i];
+	   //for(int i=0; i<n; i++)
+	   //     cout<<arr1[i]<<" ";
+	   // for(int i=0; i<m; i++)
+	   //     cout<<arr2[i]<<" ";
+	    util(arr1, arr2, n, m);
+	}
+	return 0;
+}
+
+
+
+
+
+// M2
+/*
+void util(int arr1[], int arr2[], int n, int m)
+{
+    //   for(int i=0; i<n; i++)
+	   //     cout<<arr1[i]<<" ";
+	   // for(int i=0; i<m; i++)
+	   //     cout<<arr2[i]<<" ";
     
     // cout<<median<<"\n";
     vector <int> v;
@@ -68,3 +150,4 @@ int main()
 	}
 	return 0;
 }
+*/
