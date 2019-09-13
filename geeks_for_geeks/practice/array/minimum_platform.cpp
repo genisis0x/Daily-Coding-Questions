5ddffbe145468aba0aeff4a35050f4e109c2e795
Manmeet2018 <manietdavv@gmail.com>
2019-09-14 01:30:00 +0530

    Searching_pratice

--- geeks_for_geeks/practice/array/minimum_platform.cpp
+++ geeks_for_geeks/practice/array/minimum_platform.cpp
@@ -1,5 +1,46 @@
 // PASSED
 // https://practice.geeksforgeeks.org/problems/minimum-platforms/0
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t; cin>>t;
+	while(t--)
+	{
+	    int no_of_train; cin>>no_of_train;
+	    vector<int> arrival(no_of_train);
+	    vector<int> departure(no_of_train);
+	    
+	    multimap <int, char> m_map;
+	    for(int i=0; i<no_of_train; i++)
+	    {
+	        cin>>arrival[i];
+	        m_map.insert(make_pair(arrival[i], 'a'));
+	    }
+	    for(int i=0; i<no_of_train; i++)
+	    {
+	        cin>>departure[i];
+	        m_map.insert(make_pair(departure[i], 'd'));
+	    }
+	   //  for(auto i=m_map.begin(); i!=m_map.end(); i++)
+	   //      cout<<(*i).first<<" -> "<<(*i).second<<"\n";
+	    int min_station = 0;
+	    int station = 0;
+	    for(auto i=m_map.begin(); i!=m_map.end(); i++)
+	    {
+	        if((*i).second == 'a')
+	            station++;
+	        else
+	            station--;
+	        min_station = max(station,min_station);
+	    }
+	    cout<<min_station<<"\n";
+	}
+	return 0;
+}
+
+/*
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -39,4 +80,4 @@ int main()
 	    cout<<min_station<<"\n";
 	}
 	return 0;
-}
+}*/
