952f8432c322f100f1e76fcaeb89edcb5689acd7
Manmeet2018 <manietdavv@gmail.com>
2019-09-14 01:51:07 +0530

    Searching_pratice

--- geeks_for_geeks/practice/array/leaders_in_array.cpp
+++ geeks_for_geeks/practice/array/leaders_in_array.cpp
@@ -69,3 +69,38 @@ int main()
 	return 0;
 }
 */
+
+// M3
+/*
+#include <bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+	int t; cin>>t;
+	while(t--)
+	{
+	    int n; cin>>n;
+	    vector<int> arr(n);
+	    for(int i=0; i<n; i++)
+	        cin>>arr[i];
+	    stack<int> v;
+	    int max = arr[n-1];
+	    for(int i=n-1; i>=0; i--)
+	    {
+	        if(arr[i] >= max)
+	        {
+	            max = arr[i];
+	            v.push(arr[i]);
+	        }
+	    }
+	    //reverse(v.begin(), v.end());
+	    while(!v.empty())
+	    {
+	        cout<<v.top()<<" ";
+	        v.pop();
+	    }
+	    cout<<"\n";
+	}
+	return 0;
+}
+*/
