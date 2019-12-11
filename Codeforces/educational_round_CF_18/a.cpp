436b941336a6da566f14c22bd752746f2f1bab65
Manmeet2018 <manietdavv@gmail.com>
2019-12-12 02:03:06 +0530

    Added_CF_19_educational_a_b

---
+++ Codeforces/educational_round_CF_18/a.cpp
@@ -0,0 +1,65 @@
+// Created by Manmeet Singh Parmar
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, x, n) for(ll i=x; i<(n); i++)
+#define F0R(i, n)  for(ll i=0; i<(n); i++)
+#define FORd(i, x, n) for(ll i = (n)-1; i >= x; i--)
+#define F0Rd(i,n) for(ll i = (n)-1; i >= 0; i--)
+#define FOR_B_l(i,n) for(ll i = 0; i < (1LL << n); i++)
+#define FOR_B_r(i,n) for(ll i = 0; i < (1LL >> n); i++) // typecasting of 1 in long long
+
+
+#define l_rot_n(a, n) (aLL << n) // type casting of a in long long and lef rotation of a with n;
+
+#define r_rot_n(a, n) (aLL >> n)
+
+#define mod 1000000007
+#define pi 2acos(0.0)
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back // its's faster than push_back
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+#define what_is(x) cerr << #x << "is" << x << endl;
+#define gcd(a, b) __gcd(num1 , num2)
+
+int main()
+{
+   	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin>>n; vector<int> arr(n);
+    for(int i=0; i<n; i++)
+    	cin>>arr[i];
+    sort(arr.begin(), arr.end());
+    ll min_diff = LONG_MAX;
+    for(int i=0; i<n-1; i++)
+    {
+    	ll diff = abs(arr[i+1]-arr[i]);
+    	min_diff = min(diff, min_diff);
+    }
+    ll count = 0;
+    for(int i=0; i<n-1; i++)
+    {
+    	ll diff = abs(arr[i+1]-arr[i]);
+    	count = (diff==min_diff) ? count+1: count; 
+    }
+    cout<<min_diff<<" "<<count;
+    return 0;
+}
