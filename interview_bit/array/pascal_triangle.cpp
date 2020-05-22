db16c13dde80f0692a42e689ed76b59f04359a02
Manmeet2018 <manietdavv@gmail.com>
2020-05-22 12:03:14 +0530

    refactor: Pascal_trianglesolution

--- interview_bit/array/pascal_triangle.cpp
+++ interview_bit/array/pascal_triangle.cpp
@@ -1,24 +1,18 @@
 // https://www.interviewbit.com/problems/pascal-triangle/
-vector<vector<int> > Solution::solve(int A) {
-    vector<vector<int> > a;
-    int v[A][A],i,j;
-for(i=0;i<A;i++)
-{  vector<int>c;
-    for(j=0;j<=i;j++)
-    { 
-     if((j==0)||(i==j))
-     v[i][j]=1;
-     else
-     {
-     v[i][j]=v[i-1][j-1]+v[i-1][j];
-         
-     }
-    
-        c.push_back(v[i][j]);
-    }
-a.push_back(c);
-    
-}
-return a;
-}
 
+vector<vector<int> > Solution::solve(int A) {
+    vector<vector<int> > v;
+    int a[A][A];
+    for( int i=0; i<A; i++) {
+        vector<int> t;
+        for(int j=0; j<=i; j++) {
+            if(i==j || j==0)
+                a[i][j]=1;
+            else
+                a[i][j] = a[i-1][j-1] + a[i-1][j];
+            t.push_back(a[i][j]);
+        }
+        v.push_back(t);
+    }
+    return v;
+}
