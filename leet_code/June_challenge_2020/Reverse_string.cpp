9d64a09511854d17e857ee4b70e1f0e3d5d5d7d5
Manmeet2018 <manietdavv@gmail.com>
2020-06-20 15:17:16 +0530

    feat: Added Reverse String

---
+++ leet_code/June_challenge_2020/Reverse_string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int left=0;
+        int right=n-1;
+        while(left < right) {
+            swap(s[left],s[right]);
+            left++; right--;
+        }
+    }
+};
